bb983e2818959452686c505ad8b8d3bc5ccd0ece
nick <lcl6659@163.com>
2024-11-17 21:04:47 +0800

    fix: Update UXPaymaster

--- contracts/UXPaymaster.sol
+++ contracts/UXPaymaster.sol
@@ -19,31 +19,38 @@ contract UXPaymaster is BasePaymaster, ReentrancyGuard {
     IQuoterV2 public quoterV2;
     uint24 public defaultPoolFee;
     uint256 private constant COST_OF_POST = 35000;
+    uint256 private constant PRICE_DENOMINATOR = 1e6;
+    uint256 public defaultTokenPricePerEth;
 
     event TokenUpdated(address newToken);
     event PoolFeeUpdated(uint24 newPoolFee);
     event WethUpdated(address newToken);
     event QuoterV2Updated(address newQuoterV2);
+    event TokenPricePerEthUpdate(uint256 newTokenPricePerEth);
 
-    constructor(IEntryPoint _entryPoint, IERC20 _token, IERC20 _weth, IQuoterV2 _quoterV2) BasePaymaster(_entryPoint) {
+    constructor(IEntryPoint _entryPoint, IERC20 _token, IERC20 _weth, IQuoterV2 _quoterV2, uint256 _defaultTokenPricePerEth) BasePaymaster(_entryPoint) {
         token = _token;
         weth = _weth;
         quoterV2 = _quoterV2;
-        defaultPoolFee = 3000;
+        defaultPoolFee = 500;
+        defaultTokenPricePerEth = _defaultTokenPricePerEth;
     }
 
     function setToken(IERC20 _newToken) external onlyOwner {
         token = _newToken;
         emit TokenUpdated(address(_newToken));
     }
+
     function setWeth(IERC20 _newToken) external onlyOwner {
         weth = _newToken;
         emit WethUpdated(address(_newToken));
     }
+
     function setQuoterV2(IQuoterV2 _newQuoterV2) external onlyOwner {
         quoterV2 = _newQuoterV2;
         emit QuoterV2Updated(address(_newQuoterV2));
     }
+
     function setPoolFee(uint24 _newPoolFee) external onlyOwner {
         defaultPoolFee = _newPoolFee;
         emit PoolFeeUpdated(_newPoolFee);
@@ -67,55 +74,59 @@ contract UXPaymaster is BasePaymaster, ReentrancyGuard {
         return isApprove;
     }
 
-    function getWethToTokenPrice(uint256 wethAmount) public returns (uint256) {
+    function getTokenValueOfEth(uint256 ethAmount, uint256 tokenPrice) public pure returns (uint256) {
+        return ethAmount * tokenPrice / PRICE_DENOMINATOR;
+    }
+
+    function getWethToTokenPrice(uint256 wethAmount) internal returns (uint256) {
         address wethAddress = address(weth);
         address tokenAddress = address(token);
-
         IQuoterV2.QuoteExactInputSingleParams memory quoterParams;
         quoterParams.tokenIn = wethAddress;
         quoterParams.tokenOut = tokenAddress;
         quoterParams.amountIn = wethAmount;
         quoterParams.fee = defaultPoolFee;
         quoterParams.sqrtPriceLimitX96 = 0;
-
-        (uint256 amountOut,
-        uint160 sqrtPriceX96After,
-        uint32 initializedTicksCrossed,
-        uint256 gasEstimate
-        ) = quoterV2.quoteExactInputSingle(quoterParams);
-
+        (uint256 amountOut, , , ) = quoterV2.quoteExactInputSingle(quoterParams);
         return amountOut;
     }
 
-    function _validatePaymasterUserOp(UserOperation calldata userOp, bytes32 userOpHash, uint256 maxCost)
-    internal override returns (bytes memory context, uint256 validationData) {
+    function setNewTokenPricePerEth() internal {
+        uint256 amountOut = getWethToTokenPrice(10**12);
+        require(amountOut > 0, "amountOut is zero");
+        defaultTokenPricePerEth = (amountOut * PRICE_DENOMINATOR) / (10**12);
+        emit TokenPricePerEthUpdate(defaultTokenPricePerEth);
+    }
+
+    function updateTokenPricePerEth() external onlyOwner {
+        setNewTokenPricePerEth();
+    }
+
+    function _validatePaymasterUserOp(UserOperation calldata userOp, bytes32 userOpHash, uint256 maxCost) internal view override returns (bytes memory context, uint256 validationData) {
         (userOpHash);
-        
         require(userOp.verificationGasLimit >= COST_OF_POST, "UxPaymaster: gas too low for postOp");
 
-        uint256 tokenAmount = getWethToTokenPrice(maxCost);
+        uint256 tokenPrice = defaultTokenPricePerEth;
+        uint256 tokenAmount = getTokenValueOfEth(maxCost, tokenPrice);
         require(token.balanceOf(userOp.sender) >= tokenAmount, "UxPaymaster: insufficient token balance");
 
         bool isApprove = isApproveCallData(userOp.paymasterAndData);
         if (!isApprove) {
             require(token.allowance(userOp.sender, address(this)) >= tokenAmount, "UxPaymaster: insufficient token allowance");
         }
-
         return (abi.encode(userOp.sender, tokenAmount), 0);
     }
 
     function _postOp(PostOpMode mode, bytes calldata context, uint256 actualGasCost) internal override nonReentrant {
         (address sender, uint256 preChargeTokenAmount) = abi.decode(context, (address, uint256));
-        uint256 actualTokenAmount = getWethToTokenPrice(actualGasCost);
-
+        setNewTokenPricePerEth();
+        uint256 actualTokenAmount = getTokenValueOfEth(actualGasCost, defaultTokenPricePerEth);
         if (mode != PostOpMode.postOpReverted) {
-            uint256 totalCost = Math.min(preChargeTokenAmount, actualTokenAmount + getWethToTokenPrice(COST_OF_POST));
+            uint256 totalCost = Math.min(preChargeTokenAmount, actualTokenAmount + getTokenValueOfEth(COST_OF_POST, defaultTokenPricePerEth));
             token.safeTransferFrom(sender, address(this), totalCost);
         } else {
-            uint256 postOpCost = getWethToTokenPrice(COST_OF_POST);
+            uint256 postOpCost = getTokenValueOfEth(COST_OF_POST, defaultTokenPricePerEth);
             token.safeTransferFrom(sender, address(this), postOpCost);
         }
     }
-
-    
 }
--- scripts/deployUXPaymaster.js
+++ scripts/deployUXPaymaster.js
@@ -5,18 +5,22 @@ async function main() {
 
   console.log("Deploying contracts with the owner address:", deployer.address);
 
-  const entryPointAddress = "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789";
-  const tokenAddress = "0xDaAD01cdcEC5318C7899AF31331564c6c3F3643c";
-  const wethAddress = "0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14";
-  const quoterV2Address = "0xEd1f6473345F45b75F8179591dd5bA1888cf2FB3";
+  const entryPointAddress = "0x5FF137D4b0FDCD49DcA30c7CF57E578a026d2789"; // eth-sepolia
+  const uxlinkTokenAddress = "0xDaAD01cdcEC5318C7899AF31331564c6c3F3643c"; // eth-sepolia UXLINK TOKEN
+  const wethAddress = "0xfFf9976782d46CC05630D1f6eBAb18b2324d6B14"; // eth-sepolia
+
+  const initialTokenPrice = 5108 * (10**6);
+  console.log(initialTokenPrice);
+
+  // https://docs.uniswap.org/contracts/v3/reference/deployments/ethereum-deployments
+  const quoterV2Address = "0xEd1f6473345F45b75F8179591dd5bA1888cf2FB3"; // eth-sepolia
 
   const UxPaymaster = await hre.ethers.getContractFactory("UXPaymaster");
-  const paymaster = await UxPaymaster.deploy(entryPointAddress, tokenAddress, wethAddress, quoterV2Address);
+  const paymaster = await UxPaymaster.deploy(entryPointAddress, uxlinkTokenAddress, wethAddress, quoterV2Address, initialTokenPrice);
 
   await paymaster.waitForDeployment();
 
   console.log("UxPaymaster deployed to:", paymaster.address);
-
   console.log("Deployment completed");
 }
 
